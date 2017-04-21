1d405c20d312ff1e9a90fbb5d2bd175aba04339a
Chris Svetnicka <chris@justanswer.com>
2017-04-21 11:07:48 -0700

    craft2

---
+++ craft/config/db.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+
+return array(
+
+	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+	'server' => 'vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+
+	// The name of the database to select.
+	'database' => 'xh7hiuni0v0v6tk1',
+
+	// The database username to connect with.
+	'user' => 'p8592ys0mk8fweun',
+
+	// The database password to connect with.
+	'password' => 'x7mfebng95nq4oll',
+
+	// The prefix to use when naming tables. This can be no more than 5 characters.
+	'tablePrefix' => 'craft',
+
+);
